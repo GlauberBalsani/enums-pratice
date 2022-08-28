b4568a45e524d9d790d5da36cdddec879734c656
GlauberBalsani <glauber_29@hotmail.com>
2022-08-28 19:38:58 -0300

    enums-pratice

--- src/aplication/Main.java
+++ src/aplication/Main.java
@@ -1,10 +1,31 @@
 package aplication;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+
+import entities.Worker;
+import entities.WorkerLevel;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter departament name");
+		String departamentName = sc.nextLine();
+		System.out.print("Enter worker data: ");
+		System.out.print("Name");
+		String workerName = sc.nextLine();
+		System.out.print("Level: ");
+		String workerLevel = sc.nextLine();
+		System.out.print("Base salary: ");
+		double baseSalary = sc.nextDouble();
+		
+		Worker worker = new Worker(workerName, null, null, null)
+		
+		
 	}
 
 }
--- src/entities/Departament.java
+++ src/entities/Departament.java
@@ -1,5 +1,26 @@
 package entities;
 
 public class Departament {
+	
+	private String nameString;
+	
+	public Departament() {
+	}
+
+	public Departament(String nameString) {
+		this.nameString = nameString;
+	}
+
+	public String getNameString() {
+		return nameString;
+	}
+
+	public void setNameString(String nameString) {
+		this.nameString = nameString;
+	}
+	
+	
+	
+	
 
 }
